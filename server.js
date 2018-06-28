2d7169e0ecf7a24091a32e3f5b851057aecfa548
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-06-28 17:53:02 +0100

    moved server file

--- server.js
+++ server.js
@@ -2,28 +2,19 @@ require('./config/config');
 
 const express = require('express');
 const hbs = require('hbs');
-const morgan = require('morgan')
 
 var app = express();
-// app.use(morgan('dev'));
+
 
 const port = process.env.PORT;
 
 
-
-
-app.use(bodyParser.json());
-
-
 // hbs setup
-// hbs.registerPartials(__dirname + '/../views/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 // hbs.registerHelper(__dirname, + './../views/assets');
-// app.set('view engine', 'hbs');
-//alt method to link
-// app.use(express.static(__dirname + '../public'));
-// var path = require ('path');
-// app.use(express.static(path.join(__dirname + '.../public')));
+app.set('view engine', 'hbs');
 
+app.use(express.static(__dirname + '../public'));
 
 //pages to render
 app.get('/', function(req, res) {
