88934386ccdd925f2af22e6e1e13ef41c660dbc9
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-07-01 15:08:04 +0100

    css attempt

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-require('./config/config');
-
 const express = require('express');
 const hbs = require('hbs');
-
 var app = express();
+const port = process.env.PORT || 5000;
 
 
-const port = process.env.PORT;
-
-
-// hbs setup
 hbs.registerPartials(__dirname + '/views/partials');
 // hbs.registerHelper(__dirname, + './../views/assets');
 app.set('view engine', 'hbs');
@@ -17,7 +11,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '../public'));
 
 //pages to render
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.render('index.hbs');
 });
 
@@ -29,5 +23,3 @@ app.get('/archive', (req, res) => {
 app.listen(port, () => {
     console.log(`\nRunning Express Server - port:${port}\n`);
 });
-
-module.exports = {app};
