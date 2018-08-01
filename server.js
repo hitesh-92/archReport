17d4c167ca573dea1b033800674636557b38dbf6
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-08-01 23:26:20 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const logData = require('./views/assets/loadArticles');
 const getTime = require('./views/assets/test');
