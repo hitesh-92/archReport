fadad5c63d1728f34535441890d1374280cb6223
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-10-08 17:08:39 +0100

    C:/Program Files/Git/public folder path fix

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const app = express();
 const port = process.env.PORT || 5050;
 const path = require('path');
 
-
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
-// app.use(express.static(__dirname + '../public'));
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
     res.render('index.hbs');
