ab72e88c00820ba600e42d25e84b8945198e45c5
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-21 22:03:59 +0000

    cleaned /

--- public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-async function getBtcPrice(){
-    let response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
-    let data = await response.json();
-    let price = data.bpi.USD.rate.split(".")[0];
-    document.getElementById('btcUSD').innerText = price;
-};
-
-getBtcPrice();
